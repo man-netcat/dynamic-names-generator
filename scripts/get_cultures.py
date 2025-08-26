614e4ede2ee1c78cca86bd1bc36c40e0612d7c12
Rick Teuthof <rmt22198@outlook.com>
2025-08-26 02:09:53 +0200

    Some changes to Khanates and additions to Chinese Empires

---
+++ scripts/get_cultures.py
@@ -0,0 +1,31 @@
+import json
+from pyradox import parse_file
+
+CULTURES_FILE = "/mnt/data/SteamLibrary/steamapps/common/Europa Universalis IV/common/cultures/00_cultures.txt"
+OUTPUT_FILE = "/home/rick/workspace/eu4-resources/cultures.json"
+
+# Parse the EU4 cultures file
+parsed_cultures = parse_file(CULTURES_FILE)
+
+# Filter out non-culture keys
+cultures = {
+    culture_group: [
+        top_level_key
+        for top_level_key, _ in culture_group_data.items()
+        if top_level_key
+        not in [
+            "graphical_culture",
+            "second_graphical_culture",
+            "dynasty_names",
+            "male_names",
+            "female_names",
+        ]
+    ]
+    for culture_group, culture_group_data in parsed_cultures.items()
+}
+
+# Write to JSON
+with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    json.dump(cultures, f, ensure_ascii=False, indent=4)
+
+print(f"Cultures written to {OUTPUT_FILE}")
